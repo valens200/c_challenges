c37ba49fd6a43317845c598107933a4dca70ebdc
NIYONSENGA VALENS <igihozocolombe2003@gmail.com>
2022-06-10 08:33:48 +0200

    solved exercises

---
+++ code_wars/q1.c
@@ -0,0 +1,47 @@
+#include<stdio.h>
+
+int main(){
+   int subjects;
+   int i;
+   
+   int j;
+   int k;
+   int sum;
+   int code;
+   int average;
+  int number = 3+3;
+  int total = 500;
+
+
+
+printf("enter the student code: ");
+scanf("%d",)
+printf("enter student code: ");
+scanf("%d", &code);
+printf("%d",number);
+
+printf("enter the number of subjects");
+scanf("%d",&subjects);
+
+int marks[subjects];
+printf("enter the marks :");
+
+for(i=0; i<subjects; i++){
+    scanf("%d", &marks[i]);
+}
+for(j=0; j<subjects; j++){
+   sum += marks[j];
+}
+
+for(i=0; i<subjects; i++){
+  for(j=0; j<subjects; j++){
+    printf("in the")
+  }
+}
+ int percentage = sum/total * 100;
+printf("student code: %d\n",code);
+printf("total marks : %d/%d \n", sum,total);
+printf("percentage : %f%%", percentage);
+
+return 0;
+}
